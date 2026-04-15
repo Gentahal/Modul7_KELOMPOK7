c0d94a1efc8c32ec9e01fdbd1284cee13056576b
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-15 07:21:04 +0700

    menambahkan class FilmFavourite

---
+++ FilmFavorit_103022400067.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+public class FilmFavorit
+{
+    public string title { get; set; }
+    public string director { get; set; }
+    public string year { get; set; }
+    public string genre { get; set; }
+    public double rating { get; set; }
+    public int durationMinutes { get; set; }
+
+    public bool isWatched { get; set; }
+
+    public void ReadJson()
+    {
+        string json1 = File.ReadAllText("jurnal7_1_103022400067.json");
+
+        var filmFavorit = JsonSerializer.Deserialize<FilmFavorit>(json1);
+
+        Console.WriteLine($"Title: {filmFavorit.title} Director: {director} Year: {year} Genre: {genre} Rating: {rating} DurationMinutes {durationMinutes}");
+    }
+
+}
