2b75f65721bcd23492dd995d7b3afa8534bd79f8
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-15 08:03:04 +0700

    menambahkan class GenreDictonary

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ class Program
     {
         FilmFavorit filmFavorit = new FilmFavorit();
         filmFavorit.ReadJson();
+        Console.WriteLine();
+
 
         Watchlist_103022400067 watchlist = new Watchlist_103022400067(); 
         watchlist.ReadJson();
+        Console.WriteLine();
+
+
+
+        GenreDictionary_103022400067 gdf = new GenreDictionary_103022400067();
+        gdf.ReadJson();
+        Console.WriteLine();
     }
 }
